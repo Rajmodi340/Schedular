7d973e08dd224c0cc78f697790506d4b1d6e8bbd
Raj Modi <modiraj340@gmail.com>
2025-10-17 11:04:33 +0530

    update

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,61 @@
+"use client"
+import React from 'react'
+import {useUser} from "@clerk/nextjs"
+import { CardHeader, CardTitle ,Card, CardContent} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { useForm } from 'react-hook-form'
+import {zodResolver} from "@hookform/resolvers/zod"
+import {userschema} from "@/app/lib/validator"
+import { useEffect } from 'react'
+import { updateUsername } from "@/actions/user";
+import useFetch from '@/hooks/usefetch'
+const page = () => {
+ const {isLoaded,user}= useUser()
+  const {register,handleSubmit,setValue,formState:{errors}}=useForm({
+  resolver:zodResolver(userschema),
+ })
+  useEffect(() => {
+    setValue("username", user?.username);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded]);
+ const{loading,error,fn:fnUpdateUsername}=useFetch(updateUsername)
+ const onSubmit=async(data)=>{}
+  return (
+    <div className="space-y-8">
+     <Card>
+      <CardHeader>
+        <CardTitle>
+Welcome,{user?.firstName}
+        </CardTitle>
+      </CardHeader>
+      </Card>
+      <Card>
+      <CardHeader>
+        <CardTitle>
+Your Unique Link
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <div >
+            <div className="flex items-center gap-2">
+              <span>{window?.location.origin}/</span>
+              <Input {...register("username")} placeholder="username"></Input>
+            </div>
+             {errors.username && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.username.message}
+                </p>
+             )}
+          </div>
+          <Button>Update Username</Button>
+        </form>
+      </CardContent>
+      </Card>
+
+    </div>
+  )
+}
+
+export default page
